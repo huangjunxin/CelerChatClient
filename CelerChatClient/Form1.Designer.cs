90b390ee45558cd4da8f418702eeeaf75ffdfc52
Jixzjeng <huangjunxin@live.cn>
2020-03-28 23:48:49 +0800

    First commit

---
+++ CelerChatClient/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CelerChatClient {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.chatHistoryTextBox = new System.Windows.Forms.TextBox();
+            this.chatContentTextBox = new System.Windows.Forms.TextBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chatHistoryTextBox
+            // 
+            this.chatHistoryTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.chatHistoryTextBox.Font = new System.Drawing.Font("更纱黑体 SC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.chatHistoryTextBox.Location = new System.Drawing.Point(12, 12);
+            this.chatHistoryTextBox.Multiline = true;
+            this.chatHistoryTextBox.Name = "chatHistoryTextBox";
+            this.chatHistoryTextBox.ReadOnly = true;
+            this.chatHistoryTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.chatHistoryTextBox.Size = new System.Drawing.Size(432, 306);
+            this.chatHistoryTextBox.TabIndex = 0;
+            // 
+            // chatContentTextBox
+            // 
+            this.chatContentTextBox.Font = new System.Drawing.Font("更纱黑体 SC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.chatContentTextBox.Location = new System.Drawing.Point(13, 325);
+            this.chatContentTextBox.Multiline = true;
+            this.chatContentTextBox.Name = "chatContentTextBox";
+            this.chatContentTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.chatContentTextBox.Size = new System.Drawing.Size(431, 89);
+            this.chatContentTextBox.TabIndex = 1;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(288, 420);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(369, 420);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("更纱黑体 SC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(450, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(152, 422);
+            this.listBox1.TabIndex = 4;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(207, 420);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 5;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.sendButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(614, 455);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.chatContentTextBox);
+            this.Controls.Add(this.chatHistoryTextBox);
+            this.Name = "Form1";
+            this.Text = "CelerChatClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox chatHistoryTextBox;
+        private System.Windows.Forms.TextBox chatContentTextBox;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button connectButton;
+    }
+}
+
---
+++ CelerChatClient/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CelerChatClient {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        static Socket socketClient = null;
+
+        private void connectButton_Click(object sender, EventArgs e) {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try {
+                socketClient.Connect(new IPEndPoint(ip, 10086));
+                Console.WriteLine("连接服务器成功");
+            } catch {
+                Console.WriteLine("连接服务器失败");
+                return;
+            }
+
+            // 创建接收服务器反馈的进程
+            Thread threadClient = new Thread(Recv);
+            threadClient.IsBackground = true;
+            threadClient.Start();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e) {
+            ClientSendMsg(chatContentTextBox.Text);
+            // Console.WriteLine(chatContentTextBox.Text);
+        }
+
+        public void ClientSendMsg(string targetMsg) {
+            // 清空chatContentTextBox
+            BeginInvoke(new Action(() => {
+                chatContentTextBox.Text = "";
+            }));
+
+            // 将string转换为byte
+            byte[] targetMsgBuffer = Encoding.UTF8.GetBytes(targetMsg);
+
+            // 发送消息
+            socketClient.Send(targetMsgBuffer);
+        }
+
+        public void Recv() {
+            // 创建内存缓冲区
+            byte[] srcChatHistoryBuffer = new byte[1024 * 1024];
+
+            while (true) {
+                try {
+                    int len = socketClient.Receive(srcChatHistoryBuffer);
+
+                    // 将byte转换为string
+                    string srcChatHistory = Encoding.UTF8.GetString(srcChatHistoryBuffer, 0, len);
+
+                    // Console.WriteLine(srcChatHistory);
+                    
+                    // 将收到的字符串更新到chatHistoryTextBox
+                    BeginInvoke(new Action(() => {
+                        chatHistoryTextBox.Text = srcChatHistory;
+
+                        // 滚动到最底部
+                        chatHistoryTextBox.SelectionStart = chatHistoryTextBox.Text.Length;
+                        chatHistoryTextBox.ScrollToCaret();
+                    }));
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+            }
+        }
+    }
+}
